4c3c3668e8df2fc01af16aeb6f49f171e1ee586b
Davi Dias <davifernandosousadias18@gmail.com>
2025-02-20 14:26:52 -0300

    chore: declaring new variables for colors, you need to rename them to default

--- Glicare/tailwind.config.ts
+++ Glicare/tailwind.config.ts
@@ -1,23 +1,29 @@
-import type { Config } from "tailwindcss";
+  import type { Config } from "tailwindcss";
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--color-gray)",
-        primary: "var(--color-gray)",
-        secondary: "var(--color-blue)",
-      },
-      fontFamily: {
-        roboto: "var(--font-roboto)", 
+  export default {
+    content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          background: "var(--background)",
+          foreground: "var(--foreground)",
+
+          primary: "var(--color-gray)",
+          secondary: "var(--color-blue)",
+
+          textColor: "var(--color-text-gray)",
+          
+        },
+        fontFamily: {
+          roboto: "var(--font-roboto)",
+          inter: "var(--font-inter)",
+        },
       },
     },
-  },
-  plugins: [],
-} satisfies Config;
+    plugins: [],
+  } satisfies Config;
+  
