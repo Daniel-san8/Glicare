7ce7b68b6910d6a573ddec7917c6a5be8029da5f
Daniel San <danielgdt88@hotmail.com>
2025-02-21 16:24:50 -0300

    feat: I finish creating the mobile footer

--- Glicare/src/app/components/Footer.tsx
+++ Glicare/src/app/components/Footer.tsx
@@ -2,10 +2,24 @@ import IconGlicare from "../../../public/svg/IconGlicare";
 import IconNameGlicare from "../../../public/svg/IconNameGlicare";
 
 export default function Footer() {
-    return <footer className="bg-[#4568FC]">
-        <div className="flex items-center gap-x-5 text-[#F5F5F5] pt-9 pl-6 pb-14">
+    return <footer className="bg-[#4568FC] text-[#F5F5F5] ">
+        <div className="flex items-center gap-x-5 pt-9 pl-6 pb-14">
             <IconGlicare />
             <IconNameGlicare />
         </div>
+
+        <ul className="pl-6 flex flex-col gap-y-5 pb-16 font-medium max-w-40">
+            <li className="cursor-pointer">Nosso aplicativo</li>
+            <li className="cursor-pointer">Depoimentos</li>
+            <li className="cursor-pointer">Sobre diabetes</li>
+            <li className="cursor-pointer">Contatos</li>
+            <li className="cursor-pointer">Quem somos</li>
+        </ul>
+
+        <p className="px-6 pb-10">
+            O Glicare é um aplicativo dedicado à pacientes de diabetes do tipo 2 e médicos, nutricionistas e demais profissionais da saúde que desejam maior simplicidade, praticidade e facilidade no acompanhamento, controle e evolução da doença e sintomas.
+        </p>
+
+        <p className="pl-6 pb-24">2025 © Glicare</p>
     </footer>
 }
