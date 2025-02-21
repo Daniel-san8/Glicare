0366c5722288959bda64850cf7cb6ca3c1041889
Davi Dias <davifernandosousadias18@gmail.com>
2025-02-21 17:18:19 -0300

    refactor: removed react.FC from button component and changed props

--- Glicare/src/components/ui/button.tsx
+++ Glicare/src/components/ui/button.tsx
@@ -2,27 +2,12 @@ import React, { ReactNode } from 'react';
 
 interface ButtonProps {
   children: ReactNode;
-  color?: string;
-  font?: string;
-  size?: string;
-  weight?: string;
-  width?: string | number;
-  height?: string | number; 
+  classAttributes: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ 
-  children, 
-  color = 'bg-blue-500', 
-  font = 'font-sans', 
-  size = 'text-base',
-  weight = "text-base",
-  width = 'auto', 
-  height = 'h-10' 
-}) => {
+export function Button({ children, classAttributes } : ButtonProps)  {
   return (
-    <button
-      className={`${color} ${font} ${size} ${weight} ${width} ${height} text-white rounded-lg px-4 py-2 transition-colors duration-300 hover:bg-blue-600`}
-    >
+    <button className={`${classAttributes}`}>
       {children}
     </button>
   );
