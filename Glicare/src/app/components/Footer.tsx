83b433fdb3bc47d31567fb32fbdd27f126493c0f
Daniel San <danielgdt88@hotmail.com>
2025-02-21 16:58:20 -0300

    style: I finish creating the desktop footer

--- Glicare/src/app/components/Footer.tsx
+++ Glicare/src/app/components/Footer.tsx
@@ -2,13 +2,13 @@ import IconGlicare from "../../../public/svg/IconGlicare";
 import IconNameGlicare from "../../../public/svg/IconNameGlicare";
 
 export default function Footer() {
-    return <footer className="bg-[#4568FC] text-[#F5F5F5] pl-6">
-        <div className="flex items-center gap-x-5 pt-9 pb-14">
+    return <footer className="bg-[#4568FC] text-[#F5F5F5] pl-6 lg:grid lg:grid-cols-2 lg:grid-rows-[100px 50px 100px] lg:pl-16">
+        <div className="flex items-center gap-x-5 pt-9 pb-14 lg:pb-0 lg:pt-0 lg:col-span-1 lg:max-h-14 lg:self-center">
             <IconGlicare />
             <IconNameGlicare />
         </div>
 
-        <ul className="flex flex-col gap-y-5 pb-16 font-medium max-w-40">
+        <ul className="flex flex-col gap-y-5 pb-16 font-medium max-w-40 lg:col-span-1 lg:row-span-2 lg:justify-self-center lg:pt-40">
             <li className="cursor-pointer">Nosso aplicativo</li>
             <li className="cursor-pointer">Depoimentos</li>
             <li className="cursor-pointer">Sobre diabetes</li>
@@ -16,10 +16,10 @@ export default function Footer() {
             <li className="cursor-pointer">Quem somos</li>
         </ul>
 
-        <p className="pr-6 pb-10">
+        <p className="pr-6 pb-10 lg:col-span-1 lg:pb-0 lg:pr-20 lg:row-span-1">
             O Glicare é um aplicativo dedicado à pacientes de diabetes do tipo 2 e médicos, nutricionistas e demais profissionais da saúde que desejam maior simplicidade, praticidade e facilidade no acompanhamento, controle e evolução da doença e sintomas.
         </p>
 
-        <p className="pb-24">2025 © Glicare</p>
+        <p className="pb-24 lg:col-span-2 lg:pb-11">2025 © Glicare</p>
     </footer>
 }
