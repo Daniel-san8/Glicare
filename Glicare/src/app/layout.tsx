cd47830562fe3dc4155c1bffdae28cdaf15ada1f
Daniel San <danielgdt88@hotmail.com>
2025-02-21 21:24:30 -0300

    fix: bug for deploy

--- Glicare/src/app/layout.tsx
+++ Glicare/src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/app/components/header";
-import Footer from "@/app/components/footer";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
