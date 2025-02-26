b34426a37ca42370c796e109c7ed071d7126c52a
Beatriz Santos <byalt1228@gmail.com>
2025-02-26 15:02:46 -0300

    Iserido titulo e parágrafo

---
+++ Glicare/src/app/components/who_we_are.tsx
@@ -0,0 +1,13 @@
+export default function WhoWeAre() {
+    return (
+      <section className="w-1440 h-5883 bg-primary-gray pl-6">
+        <div className="text-left px-6">
+            <h2  className="text-dark text-28 font-medium">Quem Somos</h2>
+            <p className="text-gray text-xl font-normal mt-4 w-full max-w-700">
+            Nós somos o Time Laranja, uma equipe de voluntários do projeto Pipoca Ágil, 
+            que projetamos e desenvolvemos o Glicare.
+            </p>
+        </div>
+      </section>
+    );
+  }
--- Glicare/src/app/page.tsx
+++ Glicare/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Contact from "./components/contact";
+import WhoWeAre from "./components/who_we_are";
 
 
 export default function Home() {
   return (
     <div>
-  <Contact/>
+      <Contact/>
+      <WhoWeAre/>
   </div>
   )
 }
