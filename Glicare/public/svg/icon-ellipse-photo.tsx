85c05a342beb9c2ffa8d0da4ff2dd29944d17e5e
Beatriz Santos <byalt1228@gmail.com>
2025-02-26 16:30:44 -0300

    inserido os links para o carrossel

---
+++ Glicare/public/svg/icon-ellipse-photo.tsx
@@ -0,0 +1,7 @@
+export default function IconEllipsePhoto(){
+    return(
+        <svg xmlns="http://www.w3.org/2000/svg" width="148" height="148" viewBox="0 0 148 148" fill="none">
+            <circle cx="74" cy="74" r="74" fill="#8FA8FE"/>
+        </svg>
+    );
+}
