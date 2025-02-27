1d428ab76ecf559eca6e4131e8d35b8359e283d4
Davi Dias <davifernandosousadias18@gmail.com>
2025-02-27 14:38:09 -0300

    chore: changing witdh to size created in tailwind config and correcting padding

--- Glicare/src/app/components/call-to-download.tsx
+++ Glicare/src/app/components/call-to-download.tsx
@@ -6,32 +6,41 @@ export default function CallToDownload() {
   return (
     <section>
       <div className="flex items-end mx-8 rounded-3xl bg-gradient-background h-full lg:pt-36 lg:mx-16">
-        <div className="flex flex-col justify-between text-center h-full lg:flex-row lg:items-center lg:text-start">
-          <div className="flex flex-col font-inter gap-16 p-8">
-            <div className="flex flex-col gap-4">
-              <h1 className="font-medium text-4xl text-secondaryDark lg:w-[30rem] lg:text-6xl/tight">
+        <div className="flex flex-col justify-between px-6 py-10 gap-16 text-center h-full lg:flex-row lg:items-center lg:text-start">
+          <div className="flex flex-col font-inter  ">
+            <div className="flex flex-col  gap-4">
+              <h1 className="font-medium text-4xl text-blue lg:w-480 lg:text-6xl/tight">
                 Seu controle da diabetes muito mais simples!
               </h1>
-              <p className="font-normal text-xl text-colorColorDark lg:w-[25rem]">
-                Se alimente nos horários certos, registre o histórico da sua glicemia e configure lembretes para medir a glicose.
+              <p className="font-normal text-gray text-xl lg:w-400">
+                Se alimente nos horários certos, registre o histórico da sua
+                glicemia e configure lembretes para medir a glicose.
               </p>
             </div>
             <div className="hidden lg:flex flex-col gap-4">
-              <p className="font-medium text-xl text-colorColorDark">
+              <p className="font-medium text-xl ">
                 Baixe o Glicare agora e transforme sua saúde!
               </p>
               <div className="flex gap-8 pb-10">
-                <a href="#" aria-label="Baixar na Play Store">
+                <a
+                  href="https://play.google.com/store"
+                  aria-label="Baixar na Play Store"
+                >
                   <IconPlayStore />
                 </a>
-                <a href="#" aria-label="Baixar na Apple Store">
+                <a
+                  href="https://apps.apple.com"
+                  aria-label="Baixar na Apple Store"
+                >
                   <IconAppleStore />
                 </a>
               </div>
             </div>
           </div>
-          <div className="p-8 lg:hidden">
-            <Button classAttributes="bg-secondary w-full">Baixe o Glicare</Button>
+          <div className="pt-48 lg:hidden">
+            <Button classAttributes="bg-secondary w-full">
+              Baixe o Glicare
+            </Button>
           </div>
         </div>
       </div>
