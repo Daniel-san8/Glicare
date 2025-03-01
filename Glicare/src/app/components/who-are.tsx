61fac0a5b079b4a25548aa026aadcccf5817d678
Daniel San <danielgdt88@hotmail.com>
2025-03-01 11:39:38 -0300

    feat: change icons according to function

--- Glicare/src/app/components/who-are.tsx
+++ Glicare/src/app/components/who-are.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 import ArrowNext from "@/app/components/ui/arrow-next";
 import ArrowPrev from "@/app/components/ui/arrow-prev";
 import IconLinkedin from "@/public/svg/icon-linkedin";
-import IconWeb from "@/public/svg/icon-web";
 import Slider from "react-slick";
+import IconGithub from "@/public/svg/icon-github";
+import IconWeb from "@/public/svg/icon-web";
 
 export default function WhoAre() {
   const collaborators = [
@@ -17,14 +18,16 @@ export default function WhoAre() {
       linkedin: "https://www.linkedin.com/in/carla-rovis",
       web: "portfólio",
       image: "/image/collaborators/Carla-Rovis.jpeg",
+      development: false
     },
     {
       id: 2,
-      name: "Laelson Rodrigues",
-      function: "Product Owner",
-      linkedin: "https://www.linkedin.com/in/laelson-rodrigues-qa",
-      web: "portfólio",
-      image: "/image/collaborators/Laelson-Rodrigues.jpeg",
+      name: "Daniel Santos",
+      function: "Desenvolvedor Front-end",
+      linkedin: "https://www.linkedin.com/in/daniel-san8",
+      web: "https://github.com/Daniel-san8",
+      image: "/image/collaborators/Daniel-Santos.jpeg",
+      development: true
     },
     {
       id: 3,
@@ -33,6 +36,7 @@ export default function WhoAre() {
       linkedin: "https://www.linkedin.com/in/brenneves",
       web: "https://brenoneves.com",
       image: "/image/collaborators/Breno-Neves.jpeg",
+      development: false
     },
     {
       id: 4,
@@ -41,14 +45,16 @@ export default function WhoAre() {
       linkedin: "https://www.linkedin.com/in/beatriz-souza-603379194",
       web: "https://portfolio-beatriz-ssantos.vercel.app",
       image: "/image/collaborators/Beatriz-Souza.jpeg",
+      development: true
     },
     {
       id: 5,
-      name: "Daniel Santos",
-      function: "Desenvolvedor Front-end",
-      linkedin: "https://www.linkedin.com/in/daniel-san8",
-      web: "https://github.com/Daniel-san8",
-      image: "/image/collaborators/Daniel-Santos.jpeg",
+      name: "Laelson Rodrigues",
+      function: "Product Owner",
+      linkedin: "https://www.linkedin.com/in/laelson-rodrigues-qa",
+      web: "portfólio",
+      image: "/image/collaborators/Laelson-Rodrigues.jpeg",
+      development: false
     },
     {
       id: 6,
@@ -57,25 +63,28 @@ export default function WhoAre() {
       linkedin: "https://www.linkedin.com/in/davifernandodias",
       web: "https://github.com/davifernandodias",
       image: "/image/collaborators/Davi-Dias.jpeg",
+      development: true
     },
     {
       id: 7,
       name: "Gabriel Souza",
       function: "Desenvolvedor Front-end",
-      linkedin:"https://www.linkedin.com/in/gabriel-souza-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%F0%9F%8C%88-48b741139",
+      linkedin: "https://www.linkedin.com/in/gabriel-souza-%F0%9F%8F%B3%EF%B8%8F%E2%80%8D%F0%9F%8C%88-48b741139",
       web: "https://github.com/GSOUZA11",
       image: "/image/collaborators/Gabriel-Souza.jpeg",
+      development: true
     },
   ];
 
   const settings = {
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
     nextArrow: <ArrowNext />,
     prevArrow: <ArrowPrev />,
     dots: true,
+    autoplay: true,
     responsive: [
       {
         breakpoint: 768,
@@ -137,7 +146,7 @@ export default function WhoAre() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <IconWeb />
+                  {collaborator.development ? <IconGithub /> : <IconWeb />}
                 </a>
               </div>
             </div>
