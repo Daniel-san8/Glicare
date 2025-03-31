7b6aeb331f43037a04f77447c277d612f4e79edf
Beatriz Santos <byalt1228@gmail.com>
2025-03-31 18:17:17 -0300

    Adiciona ajustes no componente CallToDownload

--- Glicare/src/app/components/call-to-download.tsx
+++ Glicare/src/app/components/call-to-download.tsx
@@ -9,19 +9,19 @@ export default function CallToDownload() {
         <div className="flex flex-col justify-between px-4 py-10 gap-16 text-center h-full lg:flex-row lg:items-center lg:text-start">
           <div className="flex flex-col lg:gap-6 font-inter  lg:px-10  ">
             <div className="flex flex-col  gap-4">
-              <h1 className="font-medium p-2 text-4xl text-blue-dark lg:w-480 lg:text-6xl/tight">
+              <h1 className="font-medium p-2 text-4xl text-blue-dark md:text-center md:text-6xl md:w-624 lg:w-480 lg:text-6xl/tight">
                 Seu controle da diabetes muito mais simples!
               </h1>
-              <p className="font-normal text-gray-dark text-xl lg:w-400">
+              <p className="font-normal text-gray-dark text-xl md:text-center md:mx-auto md:w-568 lg:w-400">
                 Se alimente nos horários certos, registre o histórico da sua
                 glicemia e configure lembretes para medir a glicose.
               </p>
             </div>
-            <div className="hidden lg:flex flex-col gap-6">
-              <p className="font-medium text-xl ">
+            <div className="hidden md:flex flex-col md:mt-32 gap-6">
+              <p className="font-medium text-xl md:text-center">
                 Baixe o Glicare agora e transforme sua saúde!
               </p>
-              <div className="flex gap-8 pb-10">
+              <div className="flex gap-8 pb-10 md:justify-center">
                 <a
                   href="https://play.google.com/store"
                   aria-label="Baixar na Play Store"
@@ -39,7 +39,7 @@ export default function CallToDownload() {
               </div>
             </div>
           </div>
-          <div className="pt-48 lg:hidden">
+          <div className="pt-48 md:hidden">
             <Button classAttributes="bg-primary w-full">
               Baixe o Glicare
             </Button>
